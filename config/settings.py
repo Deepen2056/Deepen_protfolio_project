14b4f200b3c530da580f37dd4f0974a8b5e4eeb4
Deepen2056 <deepen916@gmail.com>
2022-02-08 21:26:21 +0545

    update running file

--- config/settings.py
+++ config/settings.py
@@ -14,6 +14,11 @@ from pathlib import Path
 import os
 import django_heroku
 
+from environs import Env # new
+
+env = Env() # new
+env.read_env() # new
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,10 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--b40$1kc8s^@z$)8#+p45h=0cd@$h!ir+uugh29@vz+s+$rdnv'
+SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DEBUG", default=False)
 
 ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
@@ -80,12 +85,17 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
+DATABASES ={
+    "default": env.dj_db_url("DATABASE_URL")
 }
+
 
 
 # Password validation
